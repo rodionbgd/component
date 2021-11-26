8d1baf073414fd8231971ca6a748b20546ab3212
rodionbgd <rodionbgd@mail.ru>
2021-11-26 20:23:47 +0300

    Добавлен компонент Clicker на основе базового компонента

--- src/component.ts
+++ src/component.ts
@@ -1,62 +1,67 @@
 import TemplateManager from "./template_manager";
 
 export default class Component<State = {}> extends TemplateManager {
-  elem: HTMLElement;
+    elem: HTMLElement;
 
-  state: State | undefined;
+    state: State | undefined;
 
-  events: {
-    // eslint-disable-next-line no-unused-vars
-    [key: string]: (event?: Event) => void;
-  };
+    events: {
+        // eslint-disable-next-line no-unused-vars
+        [key: string]: (event: Event) => void;
+    };
 
-  private readonly template: string;
+    private readonly template: string;
 
-  constructor(
-    element: HTMLElement,
-    template: string,
-    initialState: Partial<State>
-  ) {
-    super();
-    this.state = initialState as State;
-    this.elem = element;
-    this.events = {};
-    this.template = template;
-  }
+    constructor(
+        element: HTMLElement,
+        template: string,
+        initialState: Partial<State>
+    ) {
+        super();
+        this.state = initialState as State;
+        this.elem = element;
+        this.events = {};
+        this.template = template;
+    }
 
-  subscribeToEvents() {
-    Object.entries(this.events).forEach(([key, value]) => {
-      const eventProps = key.split("@");
-      const event = eventProps[0];
-      const elem = eventProps[1].split(".")[0];
-      const className = eventProps[1].split(".")[1];
-      const selector = `${elem}${className ? `.${className}` : ""}`;
-      const listener = <HTMLElement>this.elem.querySelector(selector);
-      listener.addEventListener(event, value);
-    });
-  }
+    subscribeToEvents() {
+        Object.entries(this.events).forEach(([key, value]) => {
+            const eventProps = key.split("@");
+            const event = eventProps[0];
+            const elem = eventProps[1].split(".")[0];
+            const className = eventProps[1].split(".")[1];
+            const selector = `${elem}${className ? `.${className}` : ""}`;
+            const all = eventProps[1].split(".")[2]
+            if (all && all.toLowerCase() === "all") {
+                this.elem.querySelectorAll(selector).forEach(listener => listener.addEventListener(event, value));
+            } else {
+                const listener = <HTMLElement>this.elem.querySelector(selector);
+                listener.addEventListener(event, value);
+            }
+        });
+    }
 
-  setState(patch: any) {
-    Object.entries(patch).forEach(([key, value]) => {
-      (this.state as any)[key] = value;
-    });
-    this.elem.innerHTML = this.render();
-    this.subscribeToEvents();
-  }
+    setState(patch: any) {
+        Object.entries(patch).forEach(([key, value]) => {
+            (this.state as any)[key] = value;
+        });
+        this.elem.innerHTML = this.render();
+        this.subscribeToEvents();
+    }
 
-  onMount(el: HTMLElement) {
-    console.group(el.id);
-    Object.entries(this.state!).forEach(([key, value]) => {
-      console.log(`${key}: ${value}`);
-    });
-    console.groupEnd();
-  }
+    onMount(el: HTMLElement) {
+        console.group(el.id);
+        Object.entries(this.state!).forEach(([key, value]) => {
+            console.log(`${key}: ${value}`);
+        });
+        console.groupEnd();
+    }
 
-  render() {
-    const { template } = this;
-    return this.replaceVariablesValues(
-      this.replaceLoops(template, this.state),
-      this.state
-    );
-  }
-}
+    render() {
+        const {template} = this;
+        return this.replaceVariablesValues(
+            this.replaceLoops(template, this.state),
+            this.state
+        );
+    }
+};
